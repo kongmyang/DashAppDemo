401087852838223903482aea72d92ea5f1714dd7
kongmyang <kong.yang89@gmail.com>
2019-12-17 07:12:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='example-graph',
         figure={
+            #Would like the data to come from Data Lake or Azure SQL
             'data': [
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
